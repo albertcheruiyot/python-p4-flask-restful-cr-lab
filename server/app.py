5fd3b03b67ab8564560c66c87453cc4cb2427edc
Albert Cheruiyot <bettalbert99@gmail.com>
2024-07-05 14:37:55 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -17,10 +17,47 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+        
+        return response
+
+    def post(self):
+        new_record = Newsletter(
+            name=request.form['name'],
+            image=request.form['image'],
+            price=request.form['price'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(response_dict, 201)
+
+        return response
     pass
 
+api.add_resource(Plants, '/plants')
+
 class PlantByID(Resource):
+    def get(self, plant_id):
+        plant = Plant.query.filter_by(id=plant_id).first()
+
+        if not plant:
+            abort(404, message=f"Plant with ID {plant_id} not found")
+
+        return make_response(plant.to_dict(), 200)
     pass
+
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
         
 
 if __name__ == '__main__':
